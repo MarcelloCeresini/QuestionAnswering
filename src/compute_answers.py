4cebce015e9154e439be57f0c94ae3e603f97398
volpepe <volpepe.prog@gmail.com>
2022-01-23 21:46:45 +0100

    Fixed evaluation code

--- src/compute_answers.py
+++ src/compute_answers.py
@@ -1,16 +1,20 @@
 import os
-import sys
 import json
+import tensorflow as tf
 from config import Config
 import utils
 
+# For Colab:
+# DATASET_PATH = "/content/QuestionAnswering/data/training_set.json"
+# BEST_WEIGHTS_PATH = "/content/drive/MyDrive/Uni/Magistrale/NLP/Shared/ProjectWeights/training/data/training/"
+DATASET_PATH = os.path.join('..', 'data', 'dev_set.json')
+BEST_WEIGHTS_PATH = None # TODO: Add the correct path to the weights
+PATH_TO_PREDICTIONS_JSON = os.path.join('eval', 'output_predictions.json')
+
 if __name__ == '__main__':
     config = Config()
-    # Check that there is exactly one argument (the path to the
-    #   file containing the questions)
-    assert len(sys.argv) == 2, "Please, provide the position of the JSON dataset as argument"
     # Read dataset (JSON file)
-    data = utils.read_question_set(sys.argv[1])
+    data = utils.read_question_set(DATASET_PATH)
     # Process questions
     dataset = utils.create_dataset_and_ids(data, config, for_training=False)
     print("Number of samples: ", len(dataset))
@@ -19,11 +23,10 @@ if __name__ == '__main__':
     model = config.create_standard_model(hidden_state_list=[3,4,5,6])
     # Load best model weights
     #   TODO: We have no weights yet
-    BEST_WEIGHTS_PATH = "../data/training_normal/cp-0007.ckpt"
-    model.load_weights(BEST_WEIGHTS_PATH)
+    checkpoint = tf.train.Checkpoint(model)
+    checkpoint.restore(tf.train.latest_checkpoint(BEST_WEIGHTS_PATH))
     # Predict the answers to the questions in the dataset
     predictions = utils.compute_predictions(dataset, config, model)
     # Create a prediction file formatted like the one that is expected
-    PATH_TO_PREDICTIONS_JSON = os.path.join('src', 'eval', 'normal_predictions.json')
     with open(PATH_TO_PREDICTIONS_JSON, 'w') as f:
         json.dump(predictions, f)
