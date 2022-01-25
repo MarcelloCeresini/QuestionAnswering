Re-fixed answer computation code

src/compute_answers.py
@@ -1,17 +1,21 @@
 import os
 import json
+import sys
 import tensorflow as tf
 from config import Config
 import utils
 
 # For Colab:
-# DATASET_PATH = "/content/QuestionAnswering/data/training_set.json"
 # BEST_WEIGHTS_PATH = "/content/drive/MyDrive/Uni/Magistrale/NLP/Shared/ProjectWeights/training/data/training/"
-DATASET_PATH = os.path.join('..', 'data', 'dev_set.json')
-BEST_WEIGHTS_PATH = None # TODO: Add the correct path to the weights
+BEST_WEIGHTS_PATH = os.path.join('checkpoints', 'normal', 'normal.h5')  # TODO: Add the correct path to the weights
 PATH_TO_PREDICTIONS_JSON = os.path.join('eval', 'output_predictions.json')
 
 if __name__ == '__main__':
+    # Check that there is exactly one argument (the path to the file containing the questions)
+    assert len(sys.argv) == 2, "Please, provide the position of the JSON dataset as argument"
+    # Read dataset (JSON file)
+    DATASET_PATH = sys.argv[1]
+    # Create Config object
     config = Config()
     # Read dataset (JSON file)
     data = utils.read_question_set(DATASET_PATH)
@@ -20,11 +24,10 @@ if __name__ == '__main__':
     print("Number of samples: ", len(dataset))
     dataset = dataset.batch(config.BATCH_SIZE)
     # Load model
-    model = config.create_standard_model(hidden_state_list=[3,4,5,6])
+    model:tf.keras.Model = config.create_standard_model(hidden_state_list=[3,4,5,6])
     # Load best model weights
     #   TODO: We have no weights yet
-    checkpoint = tf.train.Checkpoint(model)
-    checkpoint.restore(tf.train.latest_checkpoint(BEST_WEIGHTS_PATH))
+    model.load_weights(BEST_WEIGHTS_PATH)
     # Predict the answers to the questions in the dataset
     predictions = utils.compute_predictions(dataset, config, model)
     # Create a prediction file formatted like the one that is expected